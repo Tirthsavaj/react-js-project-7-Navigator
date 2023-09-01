7c7aa19386c386532ea32a42906177448941fc5c
Tirthsavaj <tirthsavaj121@email.com>
2023-09-01 23:02:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Add';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import View from './Components/View';
+import Edit from './Components/Edit';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/viewdata' element={<View/>}/>
+              <Route path='/edit/:id' element={<Edit/>}/>
+          </Routes>
+      </BrowserRouter>
+   
   );
 }
 
---
+++ src/Components/View.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./view.css";
+
+const View = () => {
+  let all = localStorage.getItem("crud") ? JSON.parse(localStorage.getItem("crud")) : [];
+  const [alldata, setAlldata] = useState(all);
+  const [search, setSearch] = useState("");
+  const [option, setOption] = useState("");
+
+  const deleteData = (id) => {
+    let ans = alldata.filter((item) => {
+      return item.id !== id;
+    });
+    setAlldata(ans);
+  }
+
+  useEffect(() => {
+    let select = [...alldata];
+    if (option === "asc") {
+      setAlldata(select.sort((a, b) => a.name.localeCompare(b.name)));
+    } else if (option === "dsc") {
+      setAlldata(select.sort((a, b) => b.name.localeCompare(a.name)));
+    } else {
+      setAlldata(all);
+    }
+  }, [option]);
+
+  useEffect(() => {
+    let searchname = all.filter((item) => {
+      return item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+    });
+    setAlldata(searchname);
+  }, [search]);
+
+  return (
+    <div className="view-container">
+      <table className="view-table" border={1}>
+        <thead>
+          <tr className="view-header">
+            <td>Id</td>
+            <td>Name</td>
+            <td>Phone</td>
+            <td>
+                Action
+                <select className="view-select" onClick={(e) => setOption(e.target.value)}>
+                    <option>--Select--</option>
+                    <option value="asc">--Asc--</option>
+                    <option value="dsc">--Dsc--</option>
+                    <option value="all">--All--</option>
+                </select>
+
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {alldata.map((val) => {
+            const { id, name, phone } = val;
+            return (
+              <tr className="view-row" key={id}>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{phone}</td>
+                <td>
+                  <button className="view-button" onClick={() => deleteData(id)}>
+                    Delete
+                  </button>{" "}
+                  ||
+                  <button className="view-button">
+                    <Link to={`/edit/${id}`}>Edit</Link>
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Link to="/" className="view-button">
+        Add
+      </Link>
+    </div>
+  );
+};
+
+export default View;
